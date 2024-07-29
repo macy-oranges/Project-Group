332bb19de8c8f94e06fac1ebe29f5e3959674307
Macy <wonganqi888@gmail.com>
2024-07-29 15:24:51 -0700

    Initial Commit

---
+++ Project Group/Project_GroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_GroupApp.swift
+//  Project Group
+//
+//  Created by Scholar on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_GroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
